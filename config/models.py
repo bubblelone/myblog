060e1267702643116ebf649013579bb7d4da3fbc
bubblelone@163.com <bubblelone@163.com>
2019-08-11 13:13:40 +0800

    创建表

--- config/models.py
+++ config/models.py
@@ -20,6 +20,9 @@ class Link(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = '友链'
 
+    def __str__(self):
+        return self.title
+
 
 class SideBar(models.Model):
     STATUS_SHOW = 1
@@ -46,6 +49,9 @@ class SideBar(models.Model):
 
     class Meta:
         verbose_name = verbose_name_plural = '侧边栏'
+
+    def __str__(self):
+        return self.title
 
 
 
