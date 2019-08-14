129e041b5f19fed4a41ee97703b6202ab5d44493
bubblelone@163.com <bubblelone@163.com>
2019-08-14 23:03:53 +0800

    创建表

--- blog/admin.py
+++ blog/admin.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 from .models import Post, Category, Tag
+from .adminforms import PostAdminForm
 
 # Register your models here.
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
+    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count', 'owner')
     fields = ('name', 'status', 'is_nav')
 
     def save_model(self, request, obj, form, change):
@@ -23,26 +24,42 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('name', 'status', 'created_time')
+    list_display = ('name', 'status', 'created_time', 'owner')
     fields = ('name', 'status')
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(TagAdmin, self).save_model(request, obj, form, change)
 
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    '''自定义过滤器只展示当前用户分类'''
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'status', 'created_time', 'operator']
+    form = PostAdminForm
+    list_display = ['title', 'category', 'status', 'created_time', 'operator', 'owner']
     list_display_links = []
-    list_filter = ['category', ]
+    list_filter = [CategoryOwnerFilter ]
     search_fields = ['title', 'category__name']
 
     actions_on_top = True
     actions_on_bottom = True
 
+
     #编辑页面
     save_on_top = True
-
+    '''
     fields = (
         ('category', 'title'),
         'desc',
@@ -50,6 +67,36 @@ class PostAdmin(admin.ModelAdmin):
         'content',
         'tag',
     )
+    '''
+    fieldsets = (
+        (
+            '基础配置', {
+                'description': '基础配置描述',
+                'fields': (
+                    ('title', 'category'),
+                    'status',
+                ),
+            }
+        ),
+        (
+            '内容', {'fields':(
+                'desc',
+                'content',
+            ),
+
+            }
+        ),
+        (
+            '额外信息', {
+                'classes':('collapse', ),
+                'fields':('tag', ),
+            }
+        ),
+    )
+
+    #filter_horizontal = ('tag', )
+    filter_vertical = ('tag', )
+
 
     def operator(self, obj):
         return format_html(
@@ -61,3 +108,13 @@ class PostAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(PostAdmin, self).save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(owner=request.user)
+
+    class Media:
+        css = {
+            'all': ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css', ),
+        }
+        js = ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js', )
